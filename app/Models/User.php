1841507bc89403652af2829859082acbdfe0b389
Oktavionaaaa <oktavionaramadhani2810@gmail.com>
2021-11-26 15:27:11 +0700

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -41,11 +42,3 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
-// app/Models/User.php
-
-use Laravel\Sanctum\HasApiTokens;
-
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
-} 
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthorController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,24 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::get('/me','App\Http\Controllers\AuthController@me');
-Route::resource('ebook', BookController::class);
-Route::resource('author', AuthorController::class);
+
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::get('ebook', 'App\Http\Controllers\BookController@index');
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::post('ebook', 'App\Http\Controllers\BookController@store');
+    Route::put('ebook/{id}', 'App\Http\Controllers\BookController@update');
+    Route::delete('ebook/{id}', 'App\Http\Controllers\BookController@destroy');
+
+    Route::post('author', 'App\Http\Controllers\AuthorController@store');
+    Route::put('author/{id}', 'App\Http\Controllers\AuthorController@update');
+    Route::delete('author/{id}', 'App\Http\Controllers\AuthorController@destroy');
+
+
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+});
+// // Route::resource('ebook', BookController::class);
+// Route::resource('author', AuthorController::class);
+ 
